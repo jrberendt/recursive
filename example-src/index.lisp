b40a5740ebfa0387549f7757691aede92bf261db
Jack Berendt <jrberendt@gmail.com>
2024-06-18 16:51:51 -0400

    Add example-src

---
+++ example-src/index.lisp
@@ -0,0 +1,8 @@
+(tag "html"
+     (tag "head")
+     (tag "body"
+	  (tag "h1" "Hello, World!")
+	  (tag "p" "This is a simple static site generator")
+	  (tag "a href='https://github.com/jrberendt/recursive'" "Project Homepage")
+	  )
+     )
